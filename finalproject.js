77833ed9837d4d6d25e19ac560228d6925b75ca2
SungwaYu <yusungwa@gmail.com>
2021-11-12 00:08:02 -0800

    added demo track and car

--- finalproject.js
+++ finalproject.js
@@ -15,7 +15,11 @@ export class FinalProject_Base extends Scene {
             plastic: new Material(phong,
                 {ambient: .2, diffusivity: .8, specularity: .5, color: color(.9, .5, .9, 1)}),
             metal: new Material(phong,
-                {ambient: .2, diffusivity: .8, specularity: .8, color: color(.9, .5, .9, 1)})
+                {ambient: .2, diffusivity: .8, specularity: .8, color: color(.9, .5, .9, 1)}),
+            ground: new Material(new defs.Phong_Shader(),
+                {ambient: .8, diffusivity: .8, specularity: .8, color: hex_color("#e8e6e1")}),
+            car: new Material(new defs.Phong_Shader(),
+                {ambient: .8, diffusivity: .8, specularity: .8, color: hex_color("#0000FF")}),
         };
     }
 
@@ -38,7 +42,15 @@ export class FinalProject_Base extends Scene {
             // TODO
         });
         this.new_line();
+        this.key_triggered_button("Switch View", ["v"], function () {
+            // TODO
+        });
+        this.new_line();
         this.key_triggered_button("Headlight On/Off", ["h"], function () {
+            // TODO
+        });
+        this.new_line();
+        this.key_triggered_button("Map", ["m"], function () {
             // TODO
         });
     }
@@ -46,12 +58,12 @@ export class FinalProject_Base extends Scene {
     display(context, program_state) {
         if (!context.scratchpad.controls) {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
-            program_state.set_camera(Mat4.translation(0, 3, -10));
+            program_state.set_camera(Mat4.translation(-2, -4, -35));
         }
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, 1, 100);
         const t = this.t = program_state.animation_time / 1000;
-        const angle = Math.sin(t);
+        const angle = Math.sin(0);  // TODO: Math.sin(t)
         const light_position = Mat4.rotation(angle, 1, 0, 0).times(vec4(0, -1, 1, 0));
         program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
     }
@@ -61,18 +73,15 @@ export class FinalProject_Base extends Scene {
 export class FinalProject_Scene extends FinalProject_Base {
     display(context, program_state) {
         super.display(context, program_state);
-        const blue = hex_color("#1a9ffa"), yellow = hex_color("#fdc03a")
         let model_transform = Mat4.identity();
-        model_transform = model_transform.times(Mat4.translation(0, 0, 0));
-        this.shapes.box.draw(context, program_state, model_transform, this.materials.plastic.override(yellow));
-        model_transform = model_transform.times(Mat4.translation(0, -2, 0));
-        this.shapes.ball.draw(context, program_state, model_transform, this.materials.metal.override(blue));
+        model_transform = model_transform.times(Mat4.translation(0, 0, -10)).times(Mat4.scale(5, .1, 20));
+        this.shapes.box.draw(context, program_state, model_transform, this.materials.ground);
+        model_transform = Mat4.identity();
+        model_transform = model_transform.times(Mat4.translation(-15, 0, -25)).times(Mat4.scale(10, .1, 5));
+        this.shapes.box.draw(context, program_state, model_transform, this.materials.ground);
+        model_transform = Mat4.identity();
+        model_transform = model_transform.times(Mat4.translation(0, 1, 0)).times(Mat4.scale(1, .5, 2));
+        this.shapes.box.draw(context, program_state, model_transform, this.materials.car);
         const t = this.t = program_state.animation_time / 1000;
-        if (!this.hover)
-            model_transform = model_transform.times(Mat4.rotation(t, 0, 1, 0))
-        model_transform = model_transform.times(Mat4.rotation(1, 0, 0, 1))
-            .times(Mat4.scale(1, 2, 1))
-            .times(Mat4.translation(0, -1.5, 0));
-        this.shapes.box.draw(context, program_state, model_transform, this.materials.plastic.override(yellow));
     }
 }
